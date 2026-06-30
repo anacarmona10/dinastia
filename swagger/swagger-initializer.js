d6bd32fd7542732c88e30ab5fb7d1be5b72018f3
EcoTplant <ecotplant@gmail.com>
2026-06-30 15:49:49 +0000

    Documentación con Swagger

---
+++ swagger/swagger-initializer.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "../swagger.yaml",
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout",
+    deepLinking: true,
+    displayRequestDuration: true,
+    tryItOutEnabled: true
+  });
+};
