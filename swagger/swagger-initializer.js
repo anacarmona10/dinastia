d63d5ef08c9e47eb27e7a198a1c5c5a3937a6829
EcoTplant <ecotplant@gmail.com>
2026-07-01 22:04:35 +0000

    Fix swagger

--- swagger/swagger-initializer.js
+++ swagger/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   window.ui = SwaggerUIBundle({
-    url: "../swagger.yaml",
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
     presets: [
       SwaggerUIBundle.presets.apis,
