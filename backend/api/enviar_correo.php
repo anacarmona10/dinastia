479e6558cc5ac2fa8369e907e6ea96a3f497ca7b
anacarmona10 <anacarmona1023@gmail.com>
2026-08-12 16:24:04 -0500

    Creando .env y tratando de hacer que el programa envíe correos (aún no lo hace)

---
+++ backend/api/enviar_correo.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+echo "PHPMailer está funcionando correctamente.";
